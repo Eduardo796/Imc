44e86ff4ec874767dbc44f97277c4bd613011659
Eduardo Anderson <eduardoanderson767@gmail.com>
2023-09-22 21:15:55 -0300

    PGP_HTML

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+    <form action="imc.php" method="post">
+        <label for="">Peso:</label>
+        <input type="text" name="peso" value=""><br>
+        <label for="">Altura:</label>
+        <input type="text" name="altura" value=""><br>
+        <input type="submit" value="Enviar">
+    </form>
+</body>
+</html>
