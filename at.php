948e23076c34ae2986a2473b2cae813090f94943
Eduardo Anderson <eduardoanderson767@gmail.com>
2023-09-24 11:33:21 -0300

    índice de massa corporal

--- at.php
+++ at.php
@@ -3,28 +3,17 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <style>
-        form{
-
-           font-size: 20px;
-           font-family: Arial;  
-           border-radius: 50px;
-           box-shadow: 1px 1px 4px 2px red;
-           height: 50px;
-           width: 20%;
-        } 
-       
-    </style>
+    <title>Imc</title>
 </head>
 <body>
 
-<h1 class="ed">Formulário</h1>
+<h1>Formulário</h1>
+
     <form action="op.php" method="post">
-        &nbsp;&nbsp;&nbsp;<label for="nome">Nome:</label>
-        <input type="text" name="nome" id="nome"><br>
-        &nbsp;&nbsp;&nbsp;<label for="">Sobrenome:</label>
-        <input type="text" name="sobrenome" id="sobrenome">
+        <label for="">Peso:</label>
+        <input type="text" name="peso"><br>
+        <label for="">Altura:</label>
+        <input type="text" name="altura">
         <input type="submit" value="Enviar">
     </form>
 </body>
