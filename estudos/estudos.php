1a7246a73b1f8682bd95e529ea27d0ea353621b9
Eduardo Anderson <eduardoanderson767@gmail.com>
2023-09-26 19:31:58 -0300

    imc

---
+++ estudos/estudos.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Estudando</title>
+</head>
+<body>
+    <hgroup>
+        <h1> Cadastro de Produto!!!</h1><br>
+        <h2>Siga as opções abaixo para cadastrar!!!</h2><br><br>
+    </hgroup>
+
+    <form action="switch.php" method="post">
+        <label for="">Opção:</label>
+        <input type="text" name="op">
+        <label for="">Nome:</label>
+        <input type="text" name="nome">
+        <label for="">Peso:</label>
+        <input type="number" name="peso">
+    </form>
+
+</body>
+</html>
