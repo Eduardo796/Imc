49bd873d081c9c23f56ccb23cf4955696edd1f95
Eduardo Anderson <eduardoanderson767@gmail.com>
2023-09-28 21:15:29 -0300

    aa

--- estudos/pessoa.php
+++ estudos/pessoa.php
@@ -6,13 +6,20 @@ $senha= $_POST['senha'];
 $idiomas = $_POST['idiomas'];
 $gênero = $_POST['gênero'];
 
-if(isset($_POST['$idiomas'])){
-    $idiomas = $_POST['$idiomas'];
-    echo $idiomas;
-    foreach($idiomas as $idiomas){
-        print ("$idiomas");
-    }
+echo $nome; 
+echo "<br>";
+
+if($idiomas='Inglês'||$idiomas='inglês'){
+   echo "Inglês";
+}else if($idiomas='Espanhol'||$idiomas='espanhol'){
+    echo "Espanhol";
+}else if($idiomas='Francês'||$idiomas='francês'){
+    echo "Francês";
+}else{
+    echo "Opção Inválida !!!";
 }
+
+echo "<br>";
 
 if($idade>18){
     print("Maior de idade");
@@ -21,8 +28,17 @@ if($idade>18){
 }else{
     print("Idade Certa");
 }
-if($genero=='M'||$genero=='m'){
+
+echo "<br>";
+
+if($genero='M'||$genero='m'){
     echo "Masculino";
+}else if($genero='F'||$genero='f'){
+    echo "Feminino";
+}else if($genero='O'||$genero='o'){
+    echo "Outro";
+}else{
+    echo "Opção Inválida !!!";
 }
 
 ?>
